66d05f81e5e4bddb2a4e8466cce0e0e4c36d37fd
Ali Issaee <rezaa91@hotmail.co.uk>
2019-08-29 21:21:35 +0100

    3 edit/finalise content

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ import SEO from "../components/seo";
 import Button from "../components/button/button";
 import RegisterForm from "../components/form/registerForm";
 import { bounceIn, fadeInUp } from "../utils/animations";
+import twitter from "../images/svg/twitter.svg";
+import instagram from "../images/svg/instagram.svg";
+import youtube from "../images/svg/youtube.svg";
+
 
 const featuredCourses = [
   {
@@ -26,7 +30,7 @@ const IndexPage = () => {
         to={course.path}
         className="featured-course"
       >
-        <img src={course.image} />
+        <img src={course.image} alt={course.title} />
       </Link>
     ))
   )
@@ -36,9 +40,8 @@ const IndexPage = () => {
       <SEO title="Home" />
       <header className="index-header">
         <div>
-          <h1 style={bounceIn}>
-            web development. the parts they don't teach you.
-          </h1>
+          <h1 style={bounceIn}>Get Busy Coding</h1>
+          <h1 style={bounceIn}>Or Get Busy Learning</h1>
           <div className="index-action-buttons" style={fadeInUp}>
             <Link to="/courses/"><Button primary={true} className="big primary">Courses</Button></Link>
             <Link to="/blog/"><Button primary={true} className="big">Blog</Button></Link>
@@ -70,11 +73,20 @@ const IndexPage = () => {
 
       <section className="index-about-section">
         <div className="index-about-content">
-          <p className="sub-header">self taught web developer.</p>
-          <p className="big-text">I create blogs and courses tailored around web develpoment. Predominantly using JavaScript and PHP.</p>
+          <h2>Hi, My Name Is...</h2>
+          <p className="sub-header"><strong>Ali Issaee</strong>,</p>
+          <p className="big-text">I create blogs and courses tailored around web develpoment.</p>
           <p>
-            <span>Follow me:</span>
-            {/* Social media links - insta, YT, twitter */}
+            <span className="follow-me">Follow me:</span>
+            <a href="https://twitter.com/_rezaa91" className="social-media-icon" alt="twitter" target="_blank">
+              <img src={twitter} />
+            </a>
+            <a href="https://www.instagram.com/_rezaa91/" className="social-media-icon" alt="instagram" target="_blank">
+              <img src={instagram} />
+            </a>
+            <a href="https://www.youtube.com/channel/UCGPpiPgghR8uiaIIJuvKZtg" className="social-media-icon" alt="youtube" target="_blank">
+              <img src={youtube} />
+            </a>
           </p>
         </div>
       </section>
