c5caeb738f78eed27c39a3667cab0a0971e37215
Ali Issaee <rezaa91@hotmail.co.uk>
2019-09-08 20:03:29 +0100

    4 added csrf cookie in routes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+
+// set the csrf meta tag on load
+exports.onClientEntry = () => {
+  fetch("/api/")
+    .then(res => res.json())
+    .then(data => {
+      const head = document.head;
+      const csrfMeta = document.createElement('meta');
+      csrfMeta.setAttribute('name', 'csrf-token');
+      csrfMeta.setAttribute('content', data.csrfToken);
+      head.appendChild(csrfMeta);
+    })
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
-export const GET_USER = "GET_USER";
+export const LOGIN_USER = "LOGIN_USER";
 export const REGISTER_USER = "REGISTER_USER";
 export const REMOVE_USER = "REMOVE_USER";
+export const GET_USER = "GET_USER";
