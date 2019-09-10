adda26d5399fa71e16ce9f31418fc0260264cfcd
Ali Issaee <rezaa91@hotmail.co.uk>
2019-09-10 21:30:15 +0100

    0 ignore react-animation temp

--- gatsby-node.js
+++ gatsby-node.js
@@ -47,16 +47,14 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
 }
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /react-animation/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /react-animation/,
+          use: loaders.null(),
+        },
+      ],
+    },
+  })
 }
